eceb52092cf158941b1329b2d089b259592a9b66
Jumbeba <jumbeba3@gmail.com>
2021-11-17 13:29:56 -0300

    Criação e integração com banco de dados
    Utilizando o banco de dados PostgreSQL para armazenar os dados da aplicação.

--- rafa_receitas/settings.py
+++ rafa_receitas/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'rafa_receitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rafa_receita',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'Suco de limão',
+        3:'Sorvete',
+        4:'Panqueca de banana'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
