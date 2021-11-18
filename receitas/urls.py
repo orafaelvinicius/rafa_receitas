90eb3c657504eb1cf7b333903aa7c2dbcd9da7fc
Jumbeba <jumbeba3@gmail.com>
2021-11-18 14:06:56 -0300

    Exibindo as informações do banco de dados no site
    Exibindo as informações do banco de dados no site a partir do id e ajustes a partir do admin.

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita', views.receita, name='receita')
+    path('<int:receita_id>', views.receita, name='receita')
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Receita
 
 def index(request):
-    receitas = {
-        1:'Lasanha',
-        2:'Suco de limão',
-        3:'Sorvete',
-        4:'Panqueca de banana'
-    }
+    receitas = Receita.objects.all()
 
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
 
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
