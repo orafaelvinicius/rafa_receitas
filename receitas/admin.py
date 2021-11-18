ff6f5d19df9673a098321627a81564ff9e78a0d7
Jumbeba <jumbeba3@gmail.com>
2021-11-18 16:06:46 -0300

    Ajuste no admin e no html das receitas
    Ajuste no display do admin, adicionando campo de busca, filtro de categoria, e paginação ao admin.
    ajuste no html das receitas para exibir melhor os ingredientes e modo de preparo

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 4
+
+
+admin.site.register(Receita, ListandoReceitas)
 
