78b14dbdf6824f6239f1bff0cf879bbabbbde0a4
Jumbeba <jumbeba3@gmail.com>
2021-11-23 11:28:45 -0300

    Inserindo mídias e botão/página de busca

--- rafa_receitas/settings.py
+++ rafa_receitas/settings.py
@@ -129,9 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'rafa_receitas/static')
 ]
 
-# Mídias
-MIDIA_ROOT = os.path.join(BASE_DIR, 'midia')
-MIDIA_URL = '/midia/'
+# Medias
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- receitas/views.py
+++ receitas/views.py
@@ -18,3 +18,17 @@ def receita(request, receita_id):
     }
 
     return render(request, 'receita.html', receita_a_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar) #__iconteins para verificar se contem na lista
+
+    dados = {
+        'receitas': lista_receitas
+    }
+
+    return render(request, 'buscar.html', dados)
