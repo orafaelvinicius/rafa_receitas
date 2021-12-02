ac1b307b5cbe6e1a52ebb12650dc17f8a5432c17
Jumbeba <jumbeba3@gmail.com>
2021-12-02 01:22:39 -0300

    Melhorando a arquitetura

---
+++ receitas/views/__init__.py
@@ -0,0 +1,1 @@
+from .receita_views import *
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,6 @@
 import time
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
 
@@ -74,35 +73,6 @@ def dashboard(request):
     else:
         return redirect('index')
 
-def cria_receita(request):
-    if request.method == 'POST':
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
-        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes,
-                                         modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento,
-                                         categoria=categoria, foto_receita=foto_receita)
-        receita.save()
-        messages.success(request, 'Receita criada com sucesso!')
-        return redirect('dashboard')
-    else:
-        return render(request, 'usuarios/cria_receita.html')
-
-def deleta_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita.delete()
-    return redirect('dashboard')
-
-def edita_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita_a_editar = { 'receita': receita }
-    return render(request, 'usuarios/edita_receita.html', receita_a_editar)
-
 def campo_vazio(campo):
     return not campo.strip()
 
@@ -111,19 +81,3 @@ def senhas_diferentes(senha, senha2):
 
 def usuario_cadastrado(email, nome):
     return User.objects.filter(email=email).exists() or User.objects.filter(username=nome).exists()
-
-def atualiza_receita(request):
-    if request.method == 'POST':
-        receita_id = request.POST['receita_id']
-        r = Receita.objects.get(pk=receita_id)
-        r.nome_receita = request.POST['nome_receita']
-        r.ingredientes = request.POST['ingredientes']
-        r.modo_preparo = request.POST['modo_preparo']
-        r.tempo_preparo = request.POST['tempo_preparo']
-        r.rendimento = request.POST['rendimento']
-        r.categoria = request.POST['categoria']
-        if 'foto_receita' in request.FILES:
-            r.foto_receita = request.FILES['foto_receita']
-
-        r.save()
-        return redirect('dashboard')
